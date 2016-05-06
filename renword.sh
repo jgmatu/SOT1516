a0c222d5dea595d59c12d06af483632b1aa08bb9
jgmatu <fj.gutierrezs91@gmail.com>
2016-05-06 21:19:18 +0200

    renword

--- renword.sh
+++ renword.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]
+if [ $# -ne 1 ]
 then
-	ls=`ls`
-	for i in $ls; do
-		if grep '^'$1 $i > /dev/null 2> /dev/null
-		then
-			mv $i $i'.'$1;
-		fi
-	done
-	exit 0
-else
-	echo 1>&2 "You have pass bad the arguments" 
+	echo 1>&2 "You have pass bad the arguments"
 	exit 1
 fi
 
+ls=`ls`
+for i in $ls; do
+	if grep -s '^'$1 $i
+	then
+		mv $i $i'.'$1;
+	fi
+done
+exit 0
